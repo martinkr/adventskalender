465cef195bd012fa882c1e30853cdd0c5501fa84
martin krause <github@mkrause.info>
2024-10-27 21:49:22 +0200

    merges

--- app/actions.ts
+++ app/actions.ts
@@ -2,7 +2,7 @@
 
 import client from "@/lib/mongodb";
 
-export async function readDatasetForDay(day: number): Promise<WithId<Document> | null> {
+export async function readDatasetForDay(day: number): Promise<any | null> {
   // connect to MongoDB
   const mongoClient = await client.connect();
   // get the dataset
@@ -10,5 +10,4 @@ export async function readDatasetForDay(day: number): Promise<WithId<Document> |
     .db("golem-adventskalender")
     .collection("golem-adventskalender")
     .findOne({ day: day });
-
 }
