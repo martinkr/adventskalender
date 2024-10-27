2880ee25684ddd24d8327c6e9806b43ec1d1de4c
martin krause <github@mkrause.info>
2024-10-27 22:19:41 +0200

    merges

--- lib/guards.ts
+++ lib/guards.ts
@@ -1,10 +1,10 @@
 export const isValidMonth = (month: number): boolean => {
     // Add 1 as months are zero-based
     const today = new Date().getMonth() + 1;
-    return process.env.NODE_ENV === "development" ? true : today === month;
+    return process.env.DEBUG === "true" ? true : today === month;
 }
 
 export const isValidDay = (day: number): boolean => {
     const today = new Date().getDate();
-    return process.env.NODE_ENV === "development" ? true : today >= day;
+    return process.env.DEBUG === "true" ? true : today >= day;
 }
